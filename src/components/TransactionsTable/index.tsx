3eee8ded7cffc59328397e2710eb88adbd514f9d
Cauê Nishijima <caue1987@gmail.com>
2021-12-21 14:17:28 -0300

    First Commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,43 @@
+import { useTransactions } from "../../hooks/useTransactions";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  const { transactions } = useTransactions();
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td>{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {new Intl.NumberFormat('pr-BR', {
+                      style: 'currency',
+                      currency: 'BRL'
+                    }).format(transaction.amount)}
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {new Intl.DateTimeFormat('pr-BR').format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+
+      </table>
+    </Container>
+  )
+}
